3d2d911af890cc4b202548c19fe9a9676b14ad0c
s20024 <s20024@std.it-college.ac.jp>
2020-12-09 13:49:10 +0900

    marioのドットの完成

---
+++ passing/input.py
@@ -0,0 +1,14 @@
+with open("test.txt","r") as f:
+    data = f.read()
+split_data = data.split("\n")[:-1]
+# data = [a.split() for a in split_data]
+
+with open("passing.txt", "w") as f:
+    data = [i for i in split_data[0]]
+    f.write('["' + '", "'.join(data) + '"],\n')
+
+with open("passing.txt", "a") as f:
+    for a in split_data[1:]:
+        data = [i for i in a]
+        f.write('["' + '", "'.join(data) + '"],\n')
+        print(data)
